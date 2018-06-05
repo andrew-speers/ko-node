9c642f9e9e61a3b5c87a40ed2fc459031f8dd784
andrew-speers <andrew@Gamebox.localdomain>
2018-06-05 17:06:32 -0400

    Added explicit file read instructions to server.js

--- server.js
+++ server.js
@@ -14,8 +14,50 @@ var server = http.createServer(function(req, resp){
 			resp.end();
 		}
 	});
+    	fileSystem.readFile('./js/head.js', function(error, fileContent){
+		if(error){
+			resp.writeHead(500, {'Content-Type': 'text/plain'});
+			resp.end('Error');
+		}
+		else{
+			resp.writeHead(200, {'Content-Type': 'text/javascript'});
+			resp.write(fileContent);
+			resp.end();
+		}
+	});
+    	fileSystem.readFile('./js/knockout.js', function(error, fileContent){
+		if(error){
+			resp.writeHead(500, {'Content-Type': 'text/plain'});
+			resp.end('Error');
+		}
+		else{
+			resp.writeHead(200, {'Content-Type': 'text/javascript'});
+			resp.write(fileContent);
+			resp.end();
+		}
+	});
+    	fileSystem.readFile('./js/jquery.js', function(error, fileContent){
+		if(error){
+			resp.writeHead(500, {'Content-Type': 'text/plain'});
+			resp.end('Error');
+		}
+		else{
+			resp.writeHead(200, {'Content-Type': 'text/javascript'});
+			resp.write(fileContent);
+			resp.end();
+		}
+	});
+    	fileSystem.readFile('./js/app.js', function(error, fileContent){
+		if(error){
+			resp.writeHead(500, {'Content-Type': 'text/plain'});
+			resp.end('Error');
+		}
+		else{
+			resp.writeHead(200, {'Content-Type': 'text/javascript'});
+			resp.write(fileContent);
+			resp.end();
+		}
+	});
 });
 
 server.listen(8080);
-
-
