7b75a617cfb70f63b40b8506fbf9fd68a5e4173e
andrew-speers <andrew@Gamebox.localdomain>
2018-06-06 11:46:17 -0400

    Test connection

--- js/test.js
+++ js/test.js
@@ -1,10 +1,17 @@
 function fn() {
-    this.title = ko.observable("Short name")
-    this.desc  = ko.observable("Longer description")
-    this.prior = ko.observable(0)
-    this.date  = ko.observable(new Date().toJSON().slice(0,10).replace(/-/g,'/'))
+    this.title = ko.observable("Short name");
+    this.desc  = ko.observable("Longer description");
+    this.prior = ko.observable(0);
+    this.date  = ko.observable(new Date().toJSON().slice(0,10).replace(/-/g,'/'));
 
-
+    this.test = function() {
+        var data = {
+            'title': 'geoff'
+        };
+        $.post("http://jx-staging-britecore-mock-project", data, function(ret) {
+            console.log(ret);
+        });
+    };
 }
 
-ko.applyBindings(new fn())
+ko.applyBindings(new fn());
